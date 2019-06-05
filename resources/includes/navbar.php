cabb54ef4e3627a010374736c60c7b5ce76e24f2
Gavin <rockinriver2001@gmail.com>
2019-06-05 15:24:58 -0500

    Updated navbar.php
    fixed mobile

--- resources/includes/navbar.php
+++ resources/includes/navbar.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
